b7092ea2bfb982ebd559089058af712bf4e366bc
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 13:08:49 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,11 +1,11 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  SELECT MAX(gpa) FROM students;
+
 end
 
 def lowest_student_gpa
-  
+
 end
 
 def average_student_gpa
@@ -13,9 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  
+
 end
 
 def average_gpa_for_9th_grade
-  
+
 end
