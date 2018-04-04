1ddb287ca3031812eee1006150b1118ec067f2df
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 13:06:28 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  
 end
 
 def average_student_gpa
+      SELECT AVG(gpa) FROM students;
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
+  
 end
